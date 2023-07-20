cf70d1b62f0bc3b9c734258bb0a9b3a7d107db27
Amric Trudel <amrictrudel.42@gmail.com>
2023-07-20 18:58:50 +0200

    remove convoluted version of contrastive loss

--- grounding/models/clasp/clasp.py
+++ grounding/models/clasp/clasp.py
@@ -46,42 +46,6 @@ class CLASP(nn.Module):
         loss = (loss_text + loss_behav) / 2
         return loss
 
-    def contrastive_loss_complex(self, z_text, z_behavior, normalize=True) -> Tensor:
-
-        def compute_logits_and_labels(z_left, z_right):
-            labels: Tensor = torch.arange(batch_size)
-            labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
-            labels = labels.to(device)
-
-            similarity_matrix = torch.matmul(z_left, z_right.T)
-
-            # discard the main diagonal from both: labels and similarities matrix
-            mask = torch.eye(batch_size, dtype=torch.bool).to(device)
-            labels = labels[~mask].view(labels.shape[0], -1)
-            similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
-
-            positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
-            negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
-
-            logits = torch.cat([positives, negatives], dim=1)
-            labels = torch.zeros(logits.shape[0], dtype=torch.long).to(device)
-            logits /= self.temperature
-            return logits, labels
-
-        batch_size: int = z_text.shape[0]
-        if normalize:
-            z_text = F.normalize(z_text, dim=1)
-            z_behavior = F.normalize(z_behavior, dim=1)
-
-        cross_entropy = nn.CrossEntropyLoss(reduction='mean')
-        left_loss: Tensor = cross_entropy(*compute_logits_and_labels(z_text, z_behavior))
-        right_loss: Tensor = cross_entropy(*compute_logits_and_labels(z_behavior, z_text))
-
-        return (left_loss + right_loss) / 2
-
-
-
-
 
     def reparametrization_trick(self, means, log_vars):
         # Todo: log_vars or vars

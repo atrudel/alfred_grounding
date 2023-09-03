Attention prefix mapper and fix masking mistake

grounding/models/base_models/gpt2.py
@@ -2,10 +2,12 @@ from typing import List
 
 import torch
 from torch import Tensor, nn
-from transformers import GPT2TokenizerFast, BatchEncoding, GPT2LMHeadModel, GPT2Tokenizer
+from transformers import BatchEncoding, GPT2LMHeadModel, GPT2Tokenizer
 from transformers.modeling_outputs import CausalLMOutputWithCrossAttentions
 
 from config import DEVICE
+
+GPT2_EMBEDDING_SIZE = 768
 
 
 class GPT2Model(nn.Module):
@@ -19,6 +21,7 @@ class GPT2Model(nn.Module):
             param.requires_grad = False
 
     def forward(self, prefix_embeddings: Tensor, target_texts: List[str]) -> CausalLMOutputWithCrossAttentions:
+        batch_size, prefix_length, embed_size = prefix_embeddings.shape
 
         # Get embeddings associated with target text
         target_tokenized: BatchEncoding = self.tokenizer(target_texts, return_tensors='pt', padding=True)
@@ -27,15 +30,16 @@ class GPT2Model(nn.Module):
         # Concatenate prefix and target embeddings
         input_embeddings: Tensor = torch.cat([prefix_embeddings, target_embeddings], dim=1)
         input_attention_mask: Tensor = torch.cat([
-            torch.zeros(prefix_embeddings.shape[:2]),  # Prefix will be ignored in loss calculation
+            torch.ones(size=(batch_size, prefix_length)),  # Pay attention to the prefix
             target_tokenized.attention_mask
         ], dim=1).to(DEVICE)
 
         # Prepare labels (ignoring the prefix)
-        labels = torch.cat([
-            torch.full(size=prefix_embeddings.shape[:2], fill_value=-100),
+        labels: Tensor = torch.cat([
+            torch.full(size=prefix_embeddings.shape[:2], fill_value=-100),  # Ignore the prefix to calculate the loss
             target_tokenized.input_ids
         ], dim=1).to(DEVICE)
+        labels[labels == self.tokenizer.pad_token_id] = -100  # Ignore pad tokens in loss calculation
 
         output: CausalLMOutputWithCrossAttentions = self.model(
             inputs_embeds=input_embeddings,

grounding/models/base_models/prefix_mapper.py
@@ -1,19 +1,104 @@
-from torch import nn
+import torch
+from torch import nn, Tensor
+
+from models.base_models.clip import CLIP_EMBEDDING_SIZE
+from models.base_models.gpt2 import GPT2_EMBEDDING_SIZE
+
+
+class MultiheadAttention(nn.MultiheadAttention):
+    """Adaptation of pytorch's MultiheadAttention that takes care of tranforming the embeddings in Q, K, V
+    before feeding them to MHA"""
+    def __init__(self, dim_embedding: int, num_heads: int, dropout: float):
+        super().__init__(
+            embed_dim=dim_embedding,
+            num_heads=num_heads,
+            dropout=dropout,
+            bias=True,
+            batch_first=True
+        )
+        self.num_heads = num_heads
+        self.to_queries = nn.Linear(dim_embedding, dim_embedding)
+        self.to_keys_values = nn.Linear(dim_embedding, dim_embedding * 2)
+
+    def forward(self, x: Tensor):
+        batch_dim, seq_dim, embed_dim = x.shape
+        queries = self.to_queries(x)
+        keys_values = self.to_keys_values(x).reshape(batch_dim, seq_dim, 2, embed_dim)
+        keys, values = keys_values[:, :, 0], keys_values[:, :, 1]
+        output, attention_weights = super().forward(queries, keys, values)
+        return output
+
+
+class TransformerLayer(nn.Module):
+    def __init__(self, dim_embedding: int, num_heads: int, dropout: float = 0.0):
+        super().__init__()
+
+        self.mha = MultiheadAttention(dim_embedding, num_heads, dropout)
+        self.norm1 = nn.LayerNorm(dim_embedding)
+        self.norm2 = nn.LayerNorm(dim_embedding)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim_embedding, 4 * dim_embedding),
+            nn.ReLU(),
+            nn.Linear(4 * dim_embedding, dim_embedding)
+        )
+
+    def forward(self, x):
+        x = x + self.mha(self.norm1(x))[0]
+        x = x + self.mlp(self.norm2(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self,
+                 dim_embedding: int,
+                 num_heads: int,
+                 num_layers: int,
+                 dropout: float = 0.0
+                 ):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            TransformerLayer(dim_embedding, num_heads, dropout) for _ in range(num_layers)
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 
 class PrefixMapper(nn.Module):
-    def __init__(self, input_size, gpt_embed_size, k_prefix=10):
+    def __init__(self,
+                 prefix_length: int,
+                 num_layers: int = 8,
+                 dim_input: int = CLIP_EMBEDDING_SIZE,
+                 dim_output: int = GPT2_EMBEDDING_SIZE):
         super().__init__()
-        self.gpt_embed_size: int = gpt_embed_size
-        self.k_prefix: int = k_prefix
-        self.mlp = nn.Sequential(
-            nn.Linear(input_size, input_size * 2),
-            nn.ReLU(),
-            nn.Linear(input_size * 2, k_prefix * gpt_embed_size // 2),
-            nn.ReLU(),
-            nn.Linear(k_prefix * gpt_embed_size // 2, k_prefix * gpt_embed_size)
-        )
+        self.prefix_length: int = prefix_length
+        self.dim_output: int = dim_output
+        self.transformer = Transformer(dim_embedding=dim_output, num_heads=8, num_layers=num_layers)
+        self.linear = nn.Linear(dim_input, prefix_length * dim_output)
+        self.prefix_const = nn.Parameter(torch.randn(prefix_length, dim_output), requires_grad=True)
 
     def forward(self, z):
-        out = self.mlp(z)
-        return out.reshape(-1, self.k_prefix, self.gpt_embed_size)
+        z = self.linear(z).view(z.shape[0], self.prefix_length, self.dim_output)
+        constant = self.prefix_const.unsqueeze(0).expand(z.shape[0], *self.prefix_const.shape)
+        primer = torch.cat((z, constant), dim=1)
+        out = self.transformer(primer)[:, self.prefix_length:]
+        return out
+
+# class PrefixMapper(nn.Module):
+#     def __init__(self, input_size, gpt_embed_size, k_prefix=10):
+#         super().__init__()
+#         self.gpt_embed_size: int = gpt_embed_size
+#         self.k_prefix: int = k_prefix
+#         self.mlp = nn.Sequential(
+#             nn.Linear(input_size, input_size * 2),
+#             nn.ReLU(),
+#             nn.Linear(input_size * 2, k_prefix * gpt_embed_size // 2),
+#             nn.ReLU(),
+#             nn.Linear(k_prefix * gpt_embed_size // 2, k_prefix * gpt_embed_size)
+#         )
+#
+#     def forward(self, z):
+#         out = self.mlp(z)
+#         return out.reshape(-1, self.k_prefix, self.gpt_embed_size)